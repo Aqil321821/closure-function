42282e5428696636879d1be849087e468c794207
Aqil321821 <aqilrao321821@gmail.com>
2024-05-19 16:38:16 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+function num(a) {
+    return function(b) {
+        return function(c) {
+            return a + b + c;
+        };
+    };
+}
+
+console.log(num(3)(3)(3)); // Output: 9
+
+
+// a function closure 
+
+/* // A closure is a fundamental concept in JavaScript (and other programming languages)
+ that refers to a function that retains access to its lexical scope, even when the function is executed 
+ outside that scope. In simpler terms, a closure allows a function to access variables from an enclosing scope or 
+ environment, even after that scope has finished executing. */
---
+++ readme.md
@@ -0,0 +1,8 @@
+// a function closure 
+
+/* // A closure is a fundamental concept in JavaScript (and other programming languages)
+ that refers to a function that retains access to its lexical scope, even when the function is executed 
+ outside that scope. In simpler terms, a closure allows a function to access variables from an enclosing scope or 
+ environment, even after that scope has finished executing. */
+
+ 
